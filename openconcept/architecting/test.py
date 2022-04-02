a7c58b2c068777cbad39b88b52f33ea307285fea
MahmoudFouda <mahmoud.fouda@dlr.de>
2022-04-02 20:46:24 +0200

    add a test runner file to architecting module

---
+++ openconcept/architecting/test.py
@@ -0,0 +1,8 @@
+import unittest
+from builder.tests.dynamic_conventional_twin_turboprop_test import DynamicConventionalTwinTurbopropTestCase
+from builder.tests.dynamic_electric_twin_prop_test import DynamicElectricTwinTurbopropTestCase
+from builder.tests.dynamic_series_hybrid_twin_prop_test import DynamicSeriesHybridTwinTurbopropTestCase
+from builder.tests.dynamic_turboelectric_twin_prop_test import DynamicTurboelectricTwinTurbopropTestCase
+
+if __name__ == '__main__':
+    unittest.main()
