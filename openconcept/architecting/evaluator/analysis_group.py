56270ad2eb0a90d554d38c66eb02f4e0f8440fec
eytanadler <eytana@umich.edu>
2022-04-07 18:59:29 -0400

    Added mission analysis group

---
+++ openconcept/architecting/evaluator/analysis_group.py
@@ -0,0 +1,259 @@
+import numpy as np
+import openmdao.api as om
+import openconcept.api as oc
+from openconcept.architecting.builder.architecture import (
+    PropSysArch,
+    ThrustGenElements,
+    MechPowerElements,
+    Propeller,
+    Gearbox,
+    Engine,
+)
+from openconcept.architecting.builder.ac_model import DynamicACModel
+from openconcept.analysis.performance.mission_profiles import FullMissionAnalysis, BasicMission
+from examples.aircraft_data.KingAirC90GT import data as acdata
+
+
+class DynamicKingAirAnalysisGroup(om.Group):
+    """
+    Mission analysis model for the King Air with a customizable
+    propulsion system (given as an option). It uses the aerodynamics
+    and weights from the OpenConcept example HybridTwin.
+    """
+
+    def initialize(self):
+        # Default propulsion system (conventional with gearbox)
+        arch = PropSysArch(  # Conventional with gearbox
+            thrust=ThrustGenElements(
+                propellers=[Propeller("prop1"), Propeller("prop2")],
+                gearboxes=[Gearbox("gearbox1"), Gearbox("gearbox2")],
+            ),
+            mech=MechPowerElements(engines=Engine("turboshaft", power_rating=560.0)),
+        )
+        self.options.declare(name="prop_arch", default=arch, types=PropSysArch, desc="Propulsion system architecture")
+        self.options.declare(name="num_nodes", default=11, desc="Number of integration points per flight segment")
+
+    def setup(self):
+        # Take parameters that are not already defined in the DynamicACModel
+        # from the default King Air C90GT data dictionary. This information
+        # is used for aerodynamics and weights.
+        dv_comp = self.add_subsystem("dv_comp", oc.DictIndepVarComp(acdata), promotes_outputs=["*"])
+        dv_comp.add_output_from_dict("ac|aero|polar|e")
+        dv_comp.add_output_from_dict("ac|aero|polar|CD0_TO")
+        dv_comp.add_output_from_dict("ac|aero|polar|CD0_cruise")
+        dv_comp.add_output_from_dict("ac|geom|wing|S_ref")
+        dv_comp.add_output_from_dict("ac|geom|wing|AR")
+        dv_comp.add_output_from_dict("ac|geom|wing|c4sweep")
+        dv_comp.add_output_from_dict("ac|geom|wing|taper")
+        dv_comp.add_output_from_dict("ac|geom|wing|toverc")
+        dv_comp.add_output_from_dict("ac|geom|hstab|S_ref")
+        dv_comp.add_output_from_dict("ac|geom|hstab|c4_to_wing_c4")
+        dv_comp.add_output_from_dict("ac|geom|vstab|S_ref")
+        dv_comp.add_output_from_dict("ac|geom|fuselage|height")
+        dv_comp.add_output_from_dict("ac|geom|fuselage|length")
+        dv_comp.add_output_from_dict("ac|geom|fuselage|width")
+        dv_comp.add_output_from_dict("ac|geom|fuselage|S_wet")
+        dv_comp.add_output_from_dict("ac|geom|maingear|length")
+        dv_comp.add_output_from_dict("ac|geom|nosegear|length")
+        dv_comp.add_output_from_dict("ac|propulsion|engine|rating")  # used for baseline nacelle weight
+        dv_comp.add_output_from_dict("ac|q_cruise")
+        dv_comp.add_output_from_dict("ac|num_passengers_max")
+        dv_comp.add_output_from_dict("ac|weights|MTOW")
+        dv_comp.add_output_from_dict("ac|weights|MLW")
+        dv_comp.add_output_from_dict("ac|weights|W_fuel_max")
+
+        self.add_subsystem(
+            "mission",
+            FullMissionAnalysis(
+                num_nodes=11,
+                aircraft_model=DynamicACModel.factory(self.options["prop_arch"]),
+            ),
+            promotes_inputs=["*"],
+            promotes_outputs=["*"],
+        )
+
+        # Compute MTOW - fuel burn - ZFW (OEW and payload) residual to be used as a constraint in the
+        # optimization problem (> 0) when MTOW is a design variable
+        self.add_subsystem(
+            "TOW_margin",
+            oc.AddSubtractComp(
+                output_name="residual",
+                input_names=["MTOW", "fuel_used", "OEW", "payload"],
+                units="kg",
+                scaling_factors=[1, -1, -1, -1],
+            ),
+            promotes_inputs=[("MTOW", "ac|weights|MTOW"), "payload"],
+        )
+        self.connect("cruise.ac|weights|OEW", "TOW_margin.OEW")  # connect one of the OEWs from within the mission
+        self.connect("descent.fuel_used_final", "TOW_margin.fuel_used")
+
+        self.add_subsystem(
+            "aug_obj", AugmentedFBObjective(), promotes_inputs=["ac|weights|MTOW"], promotes_outputs=["mixed_objective"]
+        )
+        self.connect("descent.fuel_used_final", "aug_obj.fuel_burn")
+
+        # Promote propulsion system variables that are used in the optimization problem as DVs
+        segments = ["v0v1", "v1vr", "rotate", "v1v0", "engineoutclimb", "climb", "cruise", "descent"]
+        var_promote = [
+            (["propmodel.mech.mech1.eng_rating", "propmodel.mech.mech2.eng_rating"], "ac|propulsion|engine|rating"),
+            (["propmodel.thrust1.diameter", "propmodel.thrust2.diameter"], "ac|propulsion|propeller|diameter"),
+        ]
+        promotes_list = []
+        for segment in segments:
+            for var in var_promote:
+                for abs_var in var[0]:
+                    promotes_list.append((f"{segment}.{abs_var}", var[1]))
+        self.promotes("mission", inputs=promotes_list)
+
+
+class AugmentedFBObjective(om.ExplicitComponent):
+    def setup(self):
+        self.add_input("fuel_burn", units="kg")
+        self.add_input("ac|weights|MTOW", units="kg")
+        self.add_output("mixed_objective", units="kg")
+        self.declare_partials(["mixed_objective"], ["fuel_burn"], val=1)
+        self.declare_partials(["mixed_objective"], ["ac|weights|MTOW"], val=1 / 100)
+
+    def compute(self, inputs, outputs):
+        outputs["mixed_objective"] = inputs["fuel_burn"] + inputs["ac|weights|MTOW"] / 100
+
+
+def opt_prob(
+    prop_arch=None,
+    num_nodes=11,
+    obj={"var": "mixed_objective", "kwargs": {}},
+    prop_sys_DVs=[{"var": "ac|propulsion|propeller|diameter", "kwargs": {"lower": 2.2, "units": "m"}}],
+    prop_sys_cons=[
+        {"var": "climb.throttle", "kwargs": {"lower": 0.0, "upper": 1.0}},
+        {"var": "cruise.throttle", "kwargs": {"lower": 0.0, "upper": 1.0}},
+        {"var": "descent.throttle", "kwargs": {"lower": 0.0, "upper": 1.0}},
+    ],
+):
+    """
+    Optimization problem definition. Can be used for analyses too.
+
+    Inputs
+    ------
+    prop_arch : PropSysArch
+        Propulsion system architecture. If none is specified, it will default
+        to a conventional architecture with a gearbox.
+    num_nodes : int
+        Number of numerical integration points per flight segment, by default 11.
+    obj : dict
+        Variable to use for objective function. The format of the dictionary should be
+        {"var": <OpenMDAO variable name>, "kwargs": {<any optional keyword arguments>}}.
+        Keyword arguments include ref, ref0, etc.
+    prop_sys_DVs : list of dict
+        List of propulsion system design variables. This is a list of dictionaries, each
+        of which are in the same format as the obj dictionary.
+    prop_sys_cons : dict
+        List of propulsion system constraints. This is a list of dictionaries, each of which
+        are in the same format as the obj dictionary.
+    """
+    # Check the inputs and set defaults
+    if prop_arch is None:
+        prop_arch = PropSysArch(  # Conventional with gearbox
+            thrust=ThrustGenElements(
+                propellers=[Propeller("prop1"), Propeller("prop2")],
+                gearboxes=[Gearbox("gearbox1"), Gearbox("gearbox2")],
+            ),
+            mech=MechPowerElements(engines=Engine("turboshaft", power_rating=560.0)),
+        )
+    if "kwargs" not in obj:
+        obj["kwargs"] = {}
+    for DV in prop_sys_DVs:
+        if "kwargs" not in DV:
+            DV["kwargs"] = {}
+    for con in prop_sys_cons:
+        if "kwargs" not in con:
+            con["kwargs"] = {}
+
+    prob = om.Problem()
+    prob.model = DynamicKingAirAnalysisGroup(num_nodes=num_nodes, prop_arch=prop_arch)
+    prob.model.nonlinear_solver = om.NewtonSolver(iprint=2, err_on_non_converge=False)
+    prob.model.nonlinear_solver.linesearch = om.BoundsEnforceLS(print_bound_enforce=False)
+    prob.model.options["assembled_jac_type"] = "csc"
+    prob.model.linear_solver = om.DirectSolver(assemble_jac=True)
+    prob.model.nonlinear_solver.options["solve_subsystems"] = True
+    prob.model.nonlinear_solver.options["maxiter"] = 15
+    prob.model.nonlinear_solver.options["atol"] = 1e-8
+    prob.model.nonlinear_solver.options["rtol"] = 1e-8
+
+    # Setup the optimization
+    prob.driver = om.ScipyOptimizeDriver(optimizer="SLSQP", maxiter=200, tol=1e-8, disp=True)
+    prob.driver.options["debug_print"] = ["objs", "desvars", "nl_cons"]
+    
+    # Add the objective
+    prob.model.add_objective(obj["var"], **obj["kwargs"])
+
+    # Use the optimizer to solve for initial weight (called MTOW here) by having initial
+    # weight as a design variable and preventing the final weight from being less than
+    # the zero fuel weight (OEW + payload)
+    prob.model.add_design_var("ac|weights|MTOW", lower=2e3, ref=5e3)
+    prob.model.add_constraint("TOW_margin.residual", lower=0.0)
+
+    # Balanced field length must be at least as good as baseline
+    prob.model.add_constraint("rotate.range_final", upper=4452.0, units="ft")
+    prob.model.add_constraint("v1v0.range_final", upper=4452.0, units="ft")
+
+    # Add propulsion system design variables and constraints
+    for DV in prop_sys_DVs:
+        prob.model.add_design_var(DV["var"], **DV["kwargs"])
+    for con in prop_sys_cons:
+        prob.model.add_constraint(con["var"], **con["kwargs"])
+
+    return prob
+
+
+def set_problem_vars(prob, num_nodes=11):
+    """
+    Sets mission profile and payload values. Also sets takeoff speed
+    guesses (to improve convergence) and takeoff throttles.
+
+    Parameters
+    ----------
+    prob : OpenMDAO Problem
+        Problem that has already been setup (prob.setup() has been called).
+    num_nodes : int
+        Number of numerical integration points per flight segment, by default 11.
+    """
+    # Set required mission parameters
+    prob.set_val("ac|weights|MTOW", 10e3, units="lb")
+    prob.set_val("ac|propulsion|engine|rating", 500, units="kW")
+    prob.set_val("climb.fltcond|vs", np.ones((num_nodes,)) * 1500, units="ft/min")
+    prob.set_val("climb.fltcond|Ueas", np.ones((num_nodes,)) * 124, units="kn")
+    prob.set_val("cruise.fltcond|vs", np.ones((num_nodes,)) * 0.01, units="ft/min")
+    prob.set_val("cruise.fltcond|Ueas", np.ones((num_nodes,)) * 170, units="kn")
+    prob.set_val("descent.fltcond|vs", np.ones((num_nodes,)) * (-600), units="ft/min")
+    prob.set_val("descent.fltcond|Ueas", np.ones((num_nodes,)) * 140, units="kn")
+
+    prob.set_val("cruise|h0", 29000, units="ft")
+    prob.set_val("mission_range", 1000, units="NM")
+    prob.set_val("payload", 500, units="lb")
+
+    # (optional) guesses for takeoff speeds may help with convergence
+    prob.set_val("v0v1.fltcond|Utrue", np.ones((num_nodes)) * 50, units="kn")
+    prob.set_val("v1vr.fltcond|Utrue", np.ones((num_nodes)) * 85, units="kn")
+    prob.set_val("v1v0.fltcond|Utrue", np.ones((num_nodes)) * 85, units="kn")
+
+    # set some airplane-specific values. The throttle edits are to derate the takeoff power of the PT6A
+    prob.set_val("v0v1.throttle", np.ones((num_nodes)) * 0.75)
+    prob.set_val("v1vr.throttle", np.ones((num_nodes)) * 0.75)
+    prob.set_val("rotate.throttle", np.ones((num_nodes)) * 0.75)
+
+
+if __name__ == "__main__":
+    prob = opt_prob(prop_sys_DVs=[])
+    prob.setup()
+    set_problem_vars(prob)
+    prob.run_model()
+    # prob.run_driver()
+    om.n2(prob, show_browser=False)
+
+    # Print some results
+    vars_list = ["ac|weights|MTOW", "descent.fuel_used_final", "rotate.range_final"]
+    units = ["lb", "lb", "ft"]
+    nice_print_names = ["MTOW", "Fuel used", "TOFL (over 35ft obstacle)"]
+    print("=======================================================================")
+    for i, thing in enumerate(vars_list):
+        print(nice_print_names[i] + ": " + str(prob.get_val(thing, units=units[i])[0]) + " " + units[i])
