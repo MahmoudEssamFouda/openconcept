ed88fbb3db11533a70595d466bee6a2e7d9e0bba
eytanadler <eytana@umich.edu>
2022-05-12 17:11:07 -0400

    Added series vs. parallel hybrid comparison

--- openconcept/architecting/evaluator/postprocess/series_vs_parallel_grids.py
+++ openconcept/architecting/evaluator/postprocess/series_vs_parallel_grids.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import pickle as pkl
 import numpy as np
 import niceplots as nice
@@ -6,24 +7,17 @@ import os
 import openmdao.api as om
 
 # ================= PLOT SETTINGS =================
-archs = ["electric", "series_hybrid", "parallel_hybrid", "conventional", "turboelectric"]
-is_oneD = [
-    False,
-    False,
-    False,
-    True,
-    True,
-]  # True if only sweep over mission range and not battery spec energy (because no battery)
+archs = ["series_hybrid", "parallel_hybrid"]  # results will be (arch[1] - arch[0]) / arch[0]
 
 variable = ["mixed objective", "MTOW", "energy", "cruise DoH", "payload_frac", "batt_frac", "prop_sys_frac"]
 nice_var_name = [
-    "Fuel burn + MTOW / 100 (kg)",
-    "MTOW (kg)",
-    "Energy used (kWh)",
-    "Cruise hybridization (0 all fuel, 1 all electric)",
-    "Paylod weight / MTOW",
-    "Battery weight / MTOW",
-    "Propulsion system weight (incl. fuel and battery) / MTOW",
+    "Parallel hybrid fuel burn + MTOW / 100 relative to series (%)",
+    "Parallel hybrid MTOW relative to series (%)",
+    "Parallel hybrid energy used relative to series (%)",
+    "Parallel hybrid cruise hybridization relative to series (%)",
+    "Parallel hybrid paylod weight / MTOW relative to series (%)",
+    "Parallel hybrid battery weight / MTOW relative to series (%)",
+    "Parallel hybrid propulsion system weight (incl. fuel and battery) / MTOW relative to series (%)",
 ]
 file_var_name = ["obj", "mtow", "energy", "cruise_doh", "payload_frac", "batt_frac", "prop_sys_frac"]
 cbar_lim = [
@@ -42,7 +36,8 @@ payload = 453.592  # kg, 1000 lbs
 nice.setRCParams(dark_mode=False, set_dark_background=False)
 plt.rcParams["font.size"] = 14
 
-curDir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+curDir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+filepath_save = os.path.join(curDir, "postprocess", "figures", "series_vs_parallel_grids")
 
 # Min and max value for each variable
 var_minmax = [
@@ -59,27 +54,23 @@ x_list = np.hstack((spec_energies - x_spacing / 2, np.array([spec_energies[-1] +
 y_list = np.hstack((mission_ranges - y_spacing / 2, np.array([mission_ranges[-1] + y_spacing / 2])))
 x, y = np.meshgrid(x_list, y_list, indexing="xy")
 
-for i_arch, arch in enumerate(archs):
-    filepath = os.path.join(curDir, "data", arch)
-    filepath_save = os.path.join(curDir, "postprocess", "figures", "result_grids")
-    oneD = is_oneD[i_arch]
+for i_var in range(len(variable)):
+    var = variable[i_var]
+    var_nice = nice_var_name[i_var]
+    var_file = file_var_name[i_var]
+    data = np.zeros_like(range_grid)
+    lim = cbar_lim[i_var]
 
-    for i_var in range(len(variable)):
-        var = variable[i_var]
-        var_nice = nice_var_name[i_var]
-        var_file = file_var_name[i_var]
-        data = np.zeros_like(range_grid)
-        clim = cbar_lim[i_var]
+    # Series hybrid
+    for i in range(range_grid.shape[0]):
+        for j in range(range_grid.shape[1]):
+            for i_arch, arch in enumerate(archs):
+                filepath = os.path.join(curDir, "data", arch)
 
-        # Series hybrid
-        for i in range(range_grid.shape[0]):
-            for j in range(range_grid.shape[1]):
                 e_batt = e_batt_grid[i, j]
                 mission_range = range_grid[i, j]
 
                 filename = f"range{int(mission_range)}nmi_eBatt{int(e_batt)}"
-                if oneD:
-                    filename = f"range{int(mission_range)}nmi"
 
                 try:
                     with open(os.path.join(filepath, filename + ".pkl"), "rb") as f:
@@ -115,24 +106,35 @@ for i_arch, arch in enumerate(archs):
 
                 print(results)
 
-                # Set the min and max of the current variable
-                var_minmax[i_var] = (min(var_minmax[i_var][0], results[var]), max(var_minmax[i_var][1], results[var]))
+                if i_arch == 0:
+                    data[i, j] = results[var]
+                else:
+                    data[i, j] = (results[var] - data[i, j]) / data[i, j] * 100.
 
-                data[i, j] = results[var]
+    # Set the min and max of the current variable
+    var_minmax[i_var] = (np.min(data), np.max(data))
 
-        plt.figure(figsize=[6.4, 4.8])
-        plt.pcolormesh(x, y, data)
-        plt.xlabel("Battery specific energy (Wh/kg)")
-        plt.ylabel("Mission range (nmi)")
-        plt.xlim((x_list[0], x_list[-1]))
-        plt.ylim((y_list[0], y_list[-1]))
-        cbar = plt.colorbar()
-        plt.title(var_nice, fontsize="medium")
-        if clim is not None:
-            plt.clim(clim)
-        plt.savefig(os.path.join(filepath_save, f"{arch}_{var_file}.pdf"))
+    print("\n\n======================================================================================")
+    print(f"      Done with {var_nice}")
+    print("======================================================================================\n\n")
 
-        plt.close("all")
+    lim = np.array([np.min(data), np.max(data)])
+    lim[0] = min(-1e-10, lim[0])
+    lim[1] = max(1e-10, lim[1])
+
+    plt.figure(figsize=[6.4, 4.8])
+    plt.pcolormesh(x, y, data, cmap="coolwarm", norm=mcolors.TwoSlopeNorm(vmin=lim[0], vcenter=0., vmax=lim[1]))
+    plt.xlabel("Battery specific energy (Wh/kg)")
+    plt.ylabel("Mission range (nmi)")
+    plt.xlim((x_list[0], x_list[-1]))
+    plt.ylim((y_list[0], y_list[-1]))
+    cbar = plt.colorbar()
+    plt.title(var_nice, fontsize="medium")
+
+    plt.clim(lim)
+    plt.savefig(os.path.join(filepath_save, f"{archs[0]}_vs_{archs[1]}_{var_file}.pdf"))
+
+    plt.close("all")
 
 # Print out the mins and maxes for each var
 print(var_minmax)
