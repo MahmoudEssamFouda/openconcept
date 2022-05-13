ffd2d814d8f2e063bd981f919b1fc739eb8a9b6b
eytanadler <eytana@umich.edu>
2022-05-13 16:41:34 -0400

    Pie chart plotting script

--- openconcept/architecting/evaluator/postprocess/weight_pie_chart.py
+++ openconcept/architecting/evaluator/postprocess/weight_pie_chart.py
@@ -1,4 +1,6 @@
+from copy import deepcopy
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolor
 import niceplots as nice
 import os
 import pickle as pkl
@@ -6,23 +8,21 @@ import openmdao.api as om
 import numpy as np
 
 # ================= PLOT SETTINGS =================
-categories = ["Propulsion", "Structure", "Fuel system", "Equipment", "Fluids", "Payload"]
+categories = ["Propulsion", "Structure", "Hydraulics,\navionics, etc.", "Payload", "Other"]
 subcategories = [
-    ["Fuel", "Battery", "Propulsion system"],
-    ["Wing", "Fuselage", "Nacelle", "Empennage", "Gear"],
-    ["Fuel system"],
-    ["Equipment"],
-    ["Fluids"],
-    ["Payload"],
+    ["Battery", "Fuel", "Propulsion\nsystem"],
+    ["Wing", "Fuselage", None, None, None],  # Nones are gear, empennage, nacelle
+    [None],  # Equipment
+    [None],  # Payload
+    [None, None],  # Fuel system, fluids
 ]
 
 vars = [
-    [{"name": "fuel burn", "pkl": True}, {"name": "ac|weights|W_battery_pass", "pkl": False}, {"name": "Other propulsion"}],
-    [{"name": "climb.OEW_calc.W_wing", "pkl": False}, {"name": "climb.OEW_calc.W_fuselage", "pkl": False}, {"name": "climb.OEW_calc.W_nacelle", "pkl": False}, {"name": "climb.OEW_calc.W_empennage", "pkl": False}, {"name": "climb.OEW_calc.W_gear", "pkl": False}],
-    [{"name": "climb.OEW_calc.W_fuelsystem", "pkl": False}],
+    [{"name": "ac|weights|W_battery_pass", "pkl": False}, {"name": "fuel burn", "pkl": True}, {"name": "Other propulsion"}],
+    [{"name": "climb.OEW_calc.W_wing", "pkl": False}, {"name": "climb.OEW_calc.W_fuselage", "pkl": False}, {"name": "climb.OEW_calc.W_gear", "pkl": False}, {"name": "climb.OEW_calc.W_empennage", "pkl": False}, {"name": "climb.OEW_calc.W_nacelle", "pkl": False}],
     [{"name": "climb.OEW_calc.W_equipment", "pkl": False}],
-    [{"name": "climb.OEW_calc.W_fluids", "pkl": False}],
     [{"name": "payload", "pkl": False}],
+    [{"name": "climb.OEW_calc.W_fuelsystem", "pkl": False}, {"name": "climb.OEW_calc.W_fluids", "pkl": False}],
 ]
 
 cases = [{"range": 466, "e_batt": 522}, {"range": 300, "e_batt": 522}]
@@ -38,7 +38,50 @@ is_oneD = [
 
 payload = 453.592  # kg, 1000 lbs
 
-# ================= CREATE PLOTS =================
+# ================= COLOR DEFINITION =================
+colors = ['#56b2f0', '#F0BC4A', '#E98F51', '#D47F82', '#A97B95']  # custom colors #4FA2CE old blue
+# colors = [mcolor.rgb2hex(plt.colormaps["Set2"](i)) for i in range(len(categories))]
+alphas = np.linspace(0.2, 0.95, 10)[-1::-1]
+sub_colors = []
+for i, sublist in enumerate(subcategories):
+    sub_colors.append([])
+    for i_alpha in range(len(sublist)):
+        sub_colors[-1].append(colors[i] + float.hex(alphas[i_alpha])[4:6])
+
+
+# ================= FUNCTIONS FOR PIE CHART LAYERS =================
+def sum_categories(vals_list):
+    """
+    Sums the sublists of a 2D Python list.
+
+    Example:
+        Input: [[1], [2, 5], [1, -1]]
+        Output: [1, 7, 0]
+    """
+    result = []
+    for sublist in vals_list:
+        total = sublist[0] * 0  # keeps the type of the sublist
+        for item in sublist:
+            total += item
+        result.append(total)
+    return result
+
+
+def expand_subcategories(vals_list):
+    """
+    Flattens a 2D Python list.
+
+    Example:
+        Input: [[1], [2, 5], [1, -1]]
+        Output: [1,2, 5, 1, -1]
+    """
+    result = []
+    for sublist in vals_list:
+        for item in sublist:
+            result.append(item)
+    return result
+
+# ================= COLLECT DATA AND PLOT =================
 nice.setRCParams(dark_mode=False, set_dark_background=False)
 plt.rcParams["font.size"] = 12
 
@@ -47,7 +90,8 @@ curDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 for i_arch, arch in enumerate(archs):
     oneD = is_oneD[i_arch]
     filepath = os.path.join(curDir, "data", arch)
-    filepath_save = os.path.join(curDir, "postprocess", "figures", "result_grids")
+    filepath_save = os.path.join(curDir, "postprocess", "figures", "weight_pie_charts")
+    subcat_labels = deepcopy(subcategories)
 
     for c in cases:
         mission_range = c["range"]
@@ -75,7 +119,6 @@ for i_arch, arch in enumerate(archs):
 
         # Collect values
         vals = []
-        MTOW = 0.
         for i_cat, cat in enumerate(categories):
             vals.append([])
 
@@ -92,14 +135,54 @@ for i_arch, arch in enumerate(archs):
 
                 if subcat == "Battery":
                     vals[-1].append(W_batt)
-                elif subcat == "Propulsion system":
+                    if W_batt == 0.0:
+                        subcat_labels[i_cat][i_subcat] = None
+                elif subcat == "Propulsion\nsystem":
                     W_prop_sys = case.get_val("cruise.propulsion_system_weight", units="kg").item()
                     vals[-1].append(W_prop_sys - W_batt)
                 elif var["pkl"]:
                     vals[-1].append(results[var["name"]])
                 else:
                     vals[-1].append(case.get_val(var["name"], units="kg").item())
+                
+                if subcat == "Fuel" and vals[-1][-1] == 0.0:
+                    subcat_labels[i_cat][i_subcat] = None
 
                 vals[-1][-1] *= multiplier
 
-                MTOW += vals[-1][-1]
+        fig = plt.figure(figsize=(5, 5))
+        ax = plt.gca()
+        nice_arch = arch.capitalize().replace("_", " ")
+        size = 0.3
+        cmap = plt.colormaps["tab20c"]
+        outer_colors = cmap(np.arange(3)*4)
+        inner_colors = cmap([1, 2, 5, 6, 9, 10])
+
+        _, outer_text = ax.pie(expand_subcategories(vals), radius=1, colors=expand_subcategories(sub_colors),
+            wedgeprops=dict(width=size, edgecolor=None), textprops=dict(rotation_mode='anchor', va='center', ha='center', color="w"),
+            labels=expand_subcategories(subcat_labels), rotatelabels=False, labeldistance=.85)
+
+        _, inner_text = ax.pie(sum_categories(vals), radius=1-size-0.01, colors=colors,
+            wedgeprops=dict(width=size, edgecolor=None), textprops=dict(rotation_mode='anchor', va='center', ha='center', color="w"),
+            labels=categories, rotatelabels=False, labeldistance=.75)
+
+        ax.set(aspect="equal")
+        ax.set_title(f"{nice_arch}, {mission_range} nmi and {e_batt} Wh/kg", y=0.935)
+
+        # Manually set some font sizes so they fit better
+        inner_text[-4].set_fontsize(8)  # "Structure"
+        inner_text[-3].set_fontsize(6)  # "Equipment"
+        inner_text[-2].set_fontsize(8)  # "Payload"
+        inner_text[-1].set_fontsize(4)  # "Other"
+        outer_text[3].set_fontsize(8)  # "Wing"
+        outer_text[4].set_fontsize(8)  # "Fuselage"
+        outer_text[1].set_fontsize(10)  # "Fuel"
+        outer_text[2].set_fontsize(8)  # "Propulsion system"
+        if arch == "conventional":
+            inner_text[0].set_fontsize(10)
+
+        # Hack the matplotlib pie chart command to put text in the center
+        ax.pie([1.], radius=0.01, colors=["w"], labels=[f"MTOW\n{results['MTOW']:0.0f} kg"], textprops=dict(rotation_mode='anchor', va='center', ha='center'))
+
+        fig.savefig(os.path.join(filepath_save, f"{arch}_weight_{mission_range}nmi_eBatt{e_batt}.pdf"))
+    
